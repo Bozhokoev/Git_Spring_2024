a78d69018c4905de5eb4965756c91db662c2be41
akim <bozhokoev@gmail.com>
2024-08-09 10:19:26 +0600

    String2/DoubleChar

---
+++ src/main/java/codingbat/string2/DoubleChar.java
@@ -0,0 +1,17 @@
+package codingbat.string2;
+
+public class DoubleChar {
+    public String doubleChar(String str) {
+        if(str.isEmpty()){
+            return "";
+        }
+        String strWord = "";
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+            strWord += c;
+            strWord += c;
+        }
+        return strWord;
+
+    }
+}
